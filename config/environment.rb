a273fe193f0ac74532304944145f5318fda806fe
Vadym <vadymkoch@gmail.com>
2013-10-02 20:00:46 +0300

    Initial commit

---
+++ config/environment.rb
@@ -0,0 +1,11 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Sampleapp::Application.initialize!
+
+
+  # Force all access to the app over SSL, use Strict-Transport-Security, 
+  # and use secure cookies.
+#  config.force_ssl = true
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sampleapp::Application.config.secret_token = '82951d317899e6f5261a0149264af97bbdfadf573adeca39d0f7ed9f75b8c0279aca4af9624db615c017576471913a6e2c21ec5f0ba18edb46270bacfda017b0'
